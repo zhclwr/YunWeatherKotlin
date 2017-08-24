2be178daf065899bf973375940f78148efa6eb7d
victor <zhclwr@163.com>
2017-08-25 00:16:06 +0800

    完善城市名、城市编码……数据库的写入和读取“

--- app/src/main/java/com/victor/yunweatherkotlin/City.kt
+++ app/src/main/java/com/victor/yunweatherkotlin/City.kt
@@ -6,4 +6,7 @@ import org.litepal.crud.DataSupport
  * Created by Victor on 2017/8/21.
  */
 
-class City(var cityCode: String?, var cityName: String?) : DataSupport()
+class City(var cityCode : String,
+           var cityName : String,
+           var province : String,
+           var city : String) : DataSupport()
